d18b2532aa73cc8e84a0cada1b559321166199eb
Sean Wahl <swahl.smw@gmail.com>
2013-10-28 18:39:28 -0700

    adding file the image search and maniuplation app

---
+++ hw_08/im_gui.py
@@ -0,0 +1,21 @@
+'''
+im_gui.py
+--------------------------------------------------------------------------------------------
+An image search and manipulation tool using Traits.
+For homework 8 of AY-250.
+--------------------------------------------------------------------------------------------
+Sean Wahl
+University of California, Berkeley
+smwahl@gmail.edu
+--------------------------------------------------------------------------------------------
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+try:
+  from enthought.traits.api import HasTraits, Str, Float
+  from enthought.traits.ui.api import View, Group, Item
+except:
+  from traits.api import HasTraits, Str, Float 
+  from traitsui.api import View, Group, Item 
+
